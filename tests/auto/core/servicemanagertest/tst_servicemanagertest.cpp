91ce2831f13daff2a567d9949b2b56062b960e0c
Dominik Holland <dominik.holland@qt.io>
2022-08-04 20:19:05 +0200

    CMake: Don't use qt_internal_add_plugin in autotests
    Otherwise all static plugins in our autotests are linked to
    all applications which are linking to the interfaceframework.
    
    Change-Id: Iefcfcdda735dba42fb945a3138169df31833daeb
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Robert Griebl <robert.griebl@qt.io>
    (cherry picked from commit 468d4c3f9d947d83efe0651675c4f7a71e509527)

--- tests/auto/core/servicemanagertest/tst_servicemanagertest.cpp
+++ tests/auto/core/servicemanagertest/tst_servicemanagertest.cpp
@@ -350,7 +350,7 @@ void ServiceManagerTest::pluginLoaderTest()
     QCOMPARE(services.count(), 1);
 
     QVERIFY(manager->hasInterface("wrong_plugin"));
-    QTest::ignoreMessage(QtWarningMsg, QRegularExpression("ServiceManager::serviceObjects - failed to cast to interface from '.*wrong_plugin.*'"));
+    QTest::ignoreMessage(QtWarningMsg, QRegularExpression("ServiceManager::serviceObjects - failed to cast to interface from '.*wrong.*'"));
     QList<QIfServiceObject *> wServices = manager->findServiceByInterface("wrong_plugin");
     QCOMPARE(wServices.count(), 0);
 
@@ -358,9 +358,6 @@ void ServiceManagerTest::pluginLoaderTest()
     manager->unloadAllBackends();
     QCOMPARE(manager->rowCount(), 0);
 }
-
-Q_IMPORT_PLUGIN(SimpleStaticPlugin)
-Q_IMPORT_PLUGIN(WrongMetadataStaticPlugin)
 
 QTEST_MAIN(ServiceManagerTest)
 
