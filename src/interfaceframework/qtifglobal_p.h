caa8069e99d17d3110c20ac91c4e36c00e167316
Marc Mutz <marc.mutz@qt.io>
2023-01-11 11:15:53 +0100

    qtifglobal_p.h: remove 5.6 backports
    Support for 5.6 ended 2019-03-16.
    
    And this is the Qt 6 code, anyway.
    
    Pick-to: 6.5
    Change-Id: I3ee8e944c5db2211470f8344c498d1a3f9c50015
    Reviewed-by: Dominik Holland <dominik.holland@qt.io>

--- src/interfaceframework/qtifglobal_p.h
+++ src/interfaceframework/qtifglobal_p.h
@@ -26,25 +26,6 @@
 
 QT_BEGIN_NAMESPACE
 
-//Backport some functions to keep it working with 5.6
-#if (QT_VERSION < QT_VERSION_CHECK(5,7,0))
-namespace QtPrivate {
-template <typename T> struct QAddConst { typedef const T Type; };
-}
-
-// this adds const to non-const objects (like std::as_const)
-template <typename T>
-Q_DECL_CONSTEXPR typename QtPrivate::QAddConst<T>::Type &qAsConst(T &t) Q_DECL_NOTHROW { return t; }
-// prevent rvalue arguments:
-template <typename T>
-void qAsConst(const T &&) Q_DECL_EQ_DELETE;
-
-#ifndef qUtf16Printable
-#  define qUtf16Printable(string) \
-    static_cast<const wchar_t*>(static_cast<const void*>(QString(string).utf16()))
-#endif
-#endif
-
 #define Q_IF_BACKEND(c, b_f, err) \
 Q_D(c); \
 auto *backend = b_f; \
