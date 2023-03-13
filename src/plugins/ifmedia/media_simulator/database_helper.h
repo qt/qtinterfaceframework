81153f97d0b1382ad8419b662f879d7c51841e35
Robert Griebl <robert.griebl@qt.io>
2023-03-13 13:06:14 +0100

    Fix 6.6 QSqlDatabase deprecations
    Also, the db.commit() doesn't do anything as there is no transaction.
    
    Task-number: QTBUG-111898
    Change-Id: I42cde44cb405f42075b6943c82e82e976ebba338
    Reviewed-by: Zoltan Gera <zoltan.gera@qt.io>
    Reviewed-by: Robert Griebl <robert.griebl@qt.io>

--- src/plugins/ifmedia/media_simulator/database_helper.h
+++ src/plugins/ifmedia/media_simulator/database_helper.h
@@ -63,11 +63,13 @@ QSqlDatabase createDatabaseConnection(const QString &connectionName, const QStri
 void createMediaDatabase(const QString &dbFile)
 {
     QSqlDatabase db = createDatabaseConnection(QStringLiteral("main"), dbFile);
-    QSqlQuery query = db.exec(QStringLiteral("CREATE TABLE IF NOT EXISTS \"queue\" (\"id\" INTEGER PRIMARY KEY, \"qindex\" INTEGER, \"track_index\" INTEGER)"));
-    if (query.lastError().isValid())
-        qFatal("Couldn't create Database Tables: %s", qPrintable(query.lastError().text()));
+    QSqlQuery createQueue(db);
+    createQueue.exec(QStringLiteral("CREATE TABLE IF NOT EXISTS \"queue\" (\"id\" INTEGER PRIMARY KEY, \"qindex\" INTEGER, \"track_index\" INTEGER)"));
+    if (createQueue.lastError().isValid())
+        qFatal("Couldn't create Database Tables: %s", qPrintable(createQueue.lastError().text()));
 
-    query = db.exec("CREATE TABLE IF NOT EXISTS track "
+    QSqlQuery createTrack(db);
+    createTrack.exec("CREATE TABLE IF NOT EXISTS track "
                      "(id integer primary key, "
                      "trackName varchar(200), "
                      "albumName varchar(200), "
@@ -78,9 +80,8 @@ void createMediaDatabase(const QString &dbFile)
                      "coverArtUrl varchar(200),"
                      "UNIQUE(file))");
 
-    if (query.lastError().isValid())
-        qFatal("Couldn't create Database Tables: %s", qPrintable(query.lastError().text()));
-    db.commit();
+    if (createTrack.lastError().isValid())
+        qFatal("Couldn't create Database Tables: %s", qPrintable(createTrack.lastError().text()));
 }
 
 #endif // DATABASE_HELPER_H
