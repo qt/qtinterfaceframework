1a3f3ae31c3dd5d9355bb443d32e482f45de557d
Zoltan Gera <zoltan.gera@qt.io>
2024-11-21 15:18:51 +0200

    Tests: Fix Remote Objects Replica ODF breach
    When the same RO backend is loaded both dynamically and statically, only
    the Replica object correspondig to the used backend should be initiated.
    However, as these generated Replicas have the same name, this is an ODF
    rule-like breach between the two libraries. On Mac, the compiler
    initiates the Replica from the static library but still wants to fit
    this to the metadata coming from the dynamic library. This ends up
    having an uninitialized Replica object with a perfectly healthy metadata
    which ends in incorrect behavior.
    QtRO backend tests are separated into static a dynamic linking
    executable versions.
    
    Fixes: QTBUG-130868
    Change-Id: I21061ffe80812f0efbcbe4f9e1ecf43cb9e28529
    Pick-to: 6.8 6.8.1
    Reviewed-by: Dominik Holland <dominik.holland@qt.io>

---
+++ tests/auto/core/ifcodegen/backends/test/rotestbase.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#ifndef ROTESTBASE_H
+#define ROTESTBASE_H
+
+#include "backendstestbase.h"
+
+class RoTestBase : public BackendsTestBase
+{
+    Q_OBJECT
+
+private slots:
+    void testRemoteObjectsConfig();
+};
+
+#endif // ROTESTBASE_H
--- tests/auto/core/ifcodegen/backends/test/tst_qtro_backend.cpp
+++ tests/auto/core/ifcodegen/backends/test/tst_qtro_backend.cpp
@@ -1,13 +1,11 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-#include "backendstestbase.h"
-#include "echo.h"
-#include "echozoned.h"
+#include "rotestbase.h"
 
 using namespace Qt::StringLiterals;
 
-class QtRoBackendTest : public BackendsTestBase
+class QtRoBackendTest : public RoTestBase
 {
     Q_OBJECT
 
@@ -17,60 +15,9 @@ private slots:
         BackendsTestBase::initTestCase_data();
         QDir currentDir = QDir::current();
 
-        QTest::newRow("qtro-static-backend") << "org.example.echomodule_qtro_static" << false << false << currentDir.absoluteFilePath(u"org-example-echo-qtro-server"_s + exeSuffix);
         QTest::newRow("qtro-server") << "echo_backend_qtro" << false << false << currentDir.absoluteFilePath(u"org-example-echo-qtro-server"_s + exeSuffix);
         QTest::newRow("qtro-server asyncBackendLoading") << "echo_backend_qtro" << false << true << currentDir.absoluteFilePath(u"org-example-echo-qtro-server"_s + exeSuffix);
         QTest::newRow("qtro-simulation-server") << "echo_backend_qtro" << true << false << currentDir.absoluteFilePath(u"org-example-echo-qtro-simulation-server"_s + exeSuffix);
-    }
-
-    void testRemoteObjectsConfig()
-    {
-        if (m_isSimulationBackend)
-            QSKIP("This test is only for remoteobject");
-
-        Echo client;
-        QSignalSpy initSpy(&client, SIGNAL(isInitializedChanged(bool)));
-        QVERIFY(initSpy.isValid());
-        QVERIFY(client.startAutoDiscovery() > QIfAbstractFeature::ErrorWhileLoading);
-
-        EchoZoned zonedClient;
-        QSignalSpy zonedInitSpy(&zonedClient, SIGNAL(isInitializedChanged(bool)));
-        QVERIFY(zonedInitSpy.isValid());
-        zonedClient.setServiceObject(client.serviceObject());
-
-        // Test with same URL for all services
-        startServer({"--serverUrl", "local:myTestUrl"});
-        client.serviceObject()->updateServiceSettings(QVariantMap({{QString("connectionUrl"), QVariant("local:myTestUrl")}}));
-
-        //wait until the client has connected and initial values are set
-        WAIT_AND_COMPARE(initSpy, 1);
-        QVERIFY(client.isInitialized());
-
-        WAIT_AND_COMPARE(zonedInitSpy, 1);
-        QVERIFY(zonedClient.isInitialized());
-
-        cleanupTestData();
-        QVERIFY(!client.isInitialized());
-        QVERIFY(!zonedClient.isInitialized());
-        initSpy.clear();
-        zonedInitSpy.clear();
-        QVERIFY(client.startAutoDiscovery() > QIfAbstractFeature::ErrorWhileLoading);
-        zonedClient.setServiceObject(client.serviceObject());
-
-        // Test with conf file, which uses different URLs for different services
-        startServer({"--serverConf", QFINDTESTDATA("server.conf")});
-        client.serviceObject()->updateServiceSettings(QVariantMap({
-            {QString("org.example.echomodule.EchoZoned"), QVariantMap({{QString("connectionUrl"), QVariant("local:echozoned")}})},
-            {QString("org.example.echomodule"), QVariantMap({{QString("connectionUrl"), QVariant("local:echomoduleconf")}})}
-        }));
-
-
-        //wait until the client has connected and initial values are set
-        WAIT_AND_COMPARE(initSpy, 1);
-        QVERIFY(client.isInitialized());
-
-        WAIT_AND_COMPARE(zonedInitSpy, 1);
-        QVERIFY(zonedClient.isInitialized());
     }
 };
 
