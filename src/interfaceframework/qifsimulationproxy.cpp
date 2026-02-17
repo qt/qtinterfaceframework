76a66681885f739e68806dae3af9b058f27d1c0f
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-02-17 10:37:26 +0100

    QUIP-23: mark remaining files with default security header
    Amends 810072f3e6c9ab94e555a65c864ba1e9d93cfbfa, which identified files
    with security critical code. The rest of the code does not represent an
    interface through which data crosses a trust boundary, so can be marked
    with the default security classification.
    
    Not included in this change are the flex and qlarl generated sources.
    
    Pick-to: 6.8
    Task-number: QTBUG-135758
    Change-Id: I35f0d1f961277fa5bf2ddd01ea1f7d560b71deb2
    Reviewed-by: Zoltan Gera <zoltan.gera@qt.io>
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit 3ad05ab44286c49e0090c292366ac97498a77729)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8299b0bdd0a5df69b837543b3e2b3eacb384beb7)

--- src/interfaceframework/qifsimulationproxy.cpp
+++ src/interfaceframework/qifsimulationproxy.cpp
@@ -2,6 +2,7 @@
 // Copyright (C) 2019 Luxoft Sweden AB
 // Copyright (C) 2018 Pelagicore AG
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "qifsimulationproxy.h"
 #include "qifsimulationengine.h"
